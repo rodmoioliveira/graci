6e0c56f4dc2c0df70b3494bd155414e2d97536f0
rodmoioliveira <rodolfo.oliveira@tvglobo.com.br>
2020-08-05 20:56:08 -0300

    corrige indentação

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const {
   fromEvent,
   interval,
+  operators: { filter }
 } = rxjs;
 
 const random = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
@@ -20,20 +21,18 @@ const quality = [
   "viciada em biscoitos"
 ];
 
+let shuffle = true;
+
 const interaction = () => {
   const qDom = document.getElementById("quality");
-  const randomize = interval(300).subscribe(() => {
-    const q = quality[random(0, quality.length - 1)];
+  interval(300).pipe(filter(() => shuffle)).subscribe(() => {
+    const int = random(0, quality.length - 1);
+    const q = quality[int];
     qDom.innerHTML = `${q}!`;
   });
-
   fromEvent(document, "click").subscribe(() => {
-    randomize.unsubscribe();
-
-    setTimeout(() => {
-      interaction();
-    }, 2000)
-  });
+    shuffle = !shuffle;
+  })
 }
 
 interaction();
