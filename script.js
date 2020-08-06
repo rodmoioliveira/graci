85015872ca99f5d49af2d19249120287af78a4cd
rodmoioliveira <rodolfo.oliveira@tvglobo.com.br>
2020-08-06 15:37:29 -0300

    impede a saída da mesma qualidade consecutivamente

--- script.js
+++ script.js
@@ -20,13 +20,21 @@ const quality = [
   "especial",
   "viciada em biscoitos"
 ];
-
+let idx = quality.map((_, i) => i);
+let display = [];
 let shuffle = true;
 
 const interaction = () => {
   const qDom = document.getElementById("quality");
   interval(300).pipe(filter(() => shuffle)).subscribe(() => {
-    const int = random(0, quality.length - 1);
+    if (!idx.length) {
+      idx = quality.map((_, i) => i);
+    }
+    const i = random(0, idx.length - 1);
+    const int = idx[i];
+    display.push(int);
+    idx = [...idx.slice(0, i), ...idx.slice(i + 1)]
+
     const q = quality[int];
     qDom.innerHTML = `${q}!`;
   });
