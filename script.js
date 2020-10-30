25174dc0d1e80cec8d84b3a5dcdc4b6834cbebac
Rodolfo Moi de Oliveira <rodolfo.moi@b2wdigital.com>
2020-10-29 23:07:24 -0300

    update

--- script.js
+++ script.js
@@ -13,29 +13,20 @@ const liner_map = (in_min, in_max, out_min, out_max) => (num) =>
       out_min
   );
 
-const quotes = [
-  'linda',
-  'foda',
-  'maravilhosa',
-  'inteligente',
-  'descolada',
-  'carinhosa',
-  'guerreira',
-  'um mulherão da porra',
-  'determinada',
-  'valorosa',
-  'especial',
-  'viciada em biscoitos',
-];
 const allImages = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 const mapQuotesImages = liner_map(
   0,
-  quotes.length,
+  quotes_yo_soy.length,
   0,
   allImages.length
 );
-const initialState = quotes.map((_, i) => i);
-let idx = [...initialState];
+
+const [init_st_soy, init_st_ser] = [
+  quotes_yo_soy,
+  quotes_quiero_ser,
+].map((arr) => arr.map((_, i) => i));
+
+let [idx] = [[...init_st_soy], [...init_st_ser]];
 let shuffle = true;
 const dom_soy = document.getElementById('soy');
 const dom_ser = document.getElementById('ser');
@@ -44,17 +35,22 @@ const toogleShuffle = () => {
   shuffle = !shuffle;
 };
 
-const handle = () => {
+const aa = () => {
   if (!idx.length) {
-    idx = [...initialState];
+    idx = [...init_st_soy];
   }
   const i = random(0, idx.length - 1);
   const int = idx[i];
   idx = [...idx.slice(0, i), ...idx.slice(i + 1)];
+  return int;
+};
 
-  const q = quotes[int];
+const handle = () => {
+  const int = aa();
+
+  const q = quotes_yo_soy[int];
   const img = `./images/${mapQuotesImages(int)}.jpg`;
-  dom_soy.innerHTML = `${q}!`;
+  dom_soy.innerHTML = `${q}`;
   dom_images.src = img;
 };
 
